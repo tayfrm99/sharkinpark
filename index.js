13430d2d5885c0bb08f2ddaa6925523c8e960b30
Wam <92419665+tayfrm99@users.noreply.github.com>
2026-03-30 20:26:14 +0530

    Update health server response message

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const http = require('http'); //forhealthserver
 //health server
 http.createServer((_, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('ok');
+  res.end('i am alive burrp weasel.pages.dev');
 }).listen(10000, () => {
   console.log('Healthcheck listening on port 10000'); //10000 or 9304
 });
